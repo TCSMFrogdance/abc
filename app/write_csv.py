5ff0888c8e6ce34aaddacf07fd859ceddf7cf15e
TCSMFrogdance <58550812+TCSMFrogdance@users.noreply.github.com>
2021-03-26 23:06:39 +0700

    3

--- app/write_csv.py
+++ app/write_csv.py
@@ -1,9 +1,9 @@
 import db
 
 def csv(data):
-    x = data.to_frame()
-    x.reset_index(inplace=True)
-    x.to_frame(index=False)
-    return x
+
+    return data
 
 csv(db.get_top_country).to_csv('top_country.csv')
+
+csv(db.get_top_employee).to_csv('top_employee.csv')
