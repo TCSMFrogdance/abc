87c71f61131e29c08bebea5e39c9573e06350343
TCSMFrogdance <58550812+TCSMFrogdance@users.noreply.github.com>
2021-04-04 16:56:12 +0700

    4

--- app/write_csv.py
+++ app/write_csv.py
@@ -4,7 +4,7 @@ def csv(data):
 
     return data
 
-csv(db.get_top_country).to_csv('top_country.csv')
+csv(db.get_top_country.map('{:.2f}'.format).to_frame()).to_csv('top_country.csv')
 
 csv(db.get_top_employee).to_csv('top_employee.csv')
 
