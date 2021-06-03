0d9dcb1612f201bcbde5b1c1834fd3214fbe1baa
TCSMFrogdance <58550812+TCSMFrogdance@users.noreply.github.com>
2021-06-03 14:10:14 +0700

    12

--- ABCproject/static/js/rankchart.js
+++ ABCproject/static/js/rankchart.js
@@ -1,4 +1,4 @@
-//khai bao cac bie sau:
+// PARAM
 // path: string link api
 // title: string
 // xlabel: string
@@ -6,81 +6,82 @@
 // keylabel: string
 
 
-(function(d3) {
-    // 'use strict';
-    //khai bao kich thuoc cua svg
+// render herizontal rank chart
+const renderHerRankChart = data => {
     const titleText = title;
     const xAxisLabelText = xlabel;
-
-    const svg = d3.select(idslsvg);
+    const svg = d3.select(idsvg);
 
     const width = +svg.attr('width');
     const height = +svg.attr('height');
 
-    //render chart
-    const render = data => {
-        const xValue = d => d['Sales'];
-        const yValue = d => d[keylabel];
-        const margin = { top: 50, right: 40, bottom: 77, left: 180 };
-        const innerWidth = width - margin.left - margin.right;
-        const innerHeight = height - margin.top - margin.bottom;
+    svg.selectAll('*').remove();
 
-        const xScale = d3.scaleLinear()
-            .domain([0, d3.max(data, xValue)])
-            .range([0, innerWidth]);
-
-        const yScale = d3.scaleBand()
-            .domain(data.map(yValue))
-            .range([0, innerHeight])
-            .padding(0.1);
-
-        const g = svg.append('g')
-            .attr('transform', `translate(${margin.left},${margin.top})`);
-
-        const xAxisTickFormat = number =>
-            d3.format('.3s')(number)
-            .replace('G', 'B');
-
-        const xAxis = d3.axisBottom(xScale)
-            .tickFormat(xAxisTickFormat)
-            .tickSize(-innerHeight);
-
-        g.append('g')
-            .call(d3.axisLeft(yScale))
-            .selectAll('.domain, .tick line')
-            .remove();
-
-        const xAxisG = g.append('g').call(xAxis)
-            .attr('transform', `translate(0,${innerHeight})`);
-
-        xAxisG.select('.domain').remove();
-
-        xAxisG.append('text')
-            .attr('class', 'axis-label')
-            .attr('y', 65)
-            .attr('x', innerWidth / 2)
-            .attr('fill', 'black')
-            .text(xAxisLabelText);
-
-        g.selectAll('rect').data(data)
-            .enter().append('rect')
-            .attr('y', d => yScale(yValue(d)))
-            .attr('width', d => xScale(xValue(d)))
-            .attr('height', yScale.bandwidth())
-            .attr("fill", "#69b3a2");
-
-        g.append('text')
-            .attr('class', 'title')
-            .attr('y', -10)
-            .text(titleText);
-    };
-
-    //call render function
-    d3.json(path).then(data => {
-        data.sort(function(b, a) {
-            return a.Sales - b.Sales;
-        });
-        render(data);
+    data.sort(function(b, a) {
+        return a.Sales - b.Sales;
     });
 
-}(d3));
+    const xValue = d => d['Sales'];
+    const yValue = d => d[keylabel];
+    const margin = { top: 30, right: 10, bottom: 40, left: 120 };
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
+
+    const xScale = d3.scaleLinear()
+        .domain([0, d3.max(data, xValue)])
+        .range([0, innerWidth]);
+
+    const yScale = d3.scaleBand()
+        .domain(data.map(yValue))
+        .range([0, innerHeight])
+        .padding(0.1);
+
+    const g = svg.append('g')
+        .attr('transform', `translate(${margin.left},${margin.top})`);
+
+    const xAxisTickFormat = number =>
+        d3.format('.3s')(number)
+        .replace('G', 'B');
+
+    const xAxis = d3.axisBottom(xScale)
+        .tickFormat(xAxisTickFormat)
+        .tickSize(-innerHeight);
+
+
+    g.append('g')
+        .call(d3.axisLeft(yScale))
+        .selectAll('.domain, .tick line')
+        .remove();
+
+    const xAxisG = g.append('g').call(xAxis)
+        .attr('transform', `translate(0,${innerHeight})`)
+        .remove();
+
+    xAxisG.select('.domain').remove();
+
+    xAxisG.append('text')
+        .attr('class', 'axis-label')
+        .attr('y', 65)
+        .attr('x', innerWidth / 2)
+        .attr('fill', 'black')
+        .text(xAxisLabelText);
+
+    g.selectAll('rect').data(data)
+        .enter().append('rect')
+        .attr('y', d => yScale(yValue(d)))
+        .attr('width', d => xScale(xValue(d)))
+        .attr('height', yScale.bandwidth())
+        .attr("fill", "#69b3a2");
+
+    g.append('text')
+        .attr('class', 'title')
+        .attr('y', -10)
+        .text(titleText);
+};
+
+//call render function
+let rankChart = path => {
+    d3.json(path).then(data => {
+        renderHerRankChart(data);
+    });
+}
